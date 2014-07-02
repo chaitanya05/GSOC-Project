19cd257fd52fcb415e9a57de1f74f482bee0ebd6
root <root@sai-Dell-System-XPS-L502X.(none)>
2014-07-02 23:49:23 +0530

    june_2_commit_20

--- displayResults.php
+++ displayResults.php
@@ -11,5 +11,35 @@ function js2php_proc() {
 }
 js2php_proc();
 ?>
+
+<?php
+
+$dbconn=pg_connect("dbname=demo user=user password=user");
+
+if(!$dbconn) {
+        echo "Error connecting to the database!<br> " ;
+        printf("%s", pg_errormessage($dbconn));
+        exit();
+}
+else {
+        echo "connected", "\n";
+}
+
+$sqlcom="select * from demo";
+$dbres = pg_exec($dbconn, $sqlcom);
+if(!$dbres) {
+        echo "Error : " + pg_errormessage($dbconn);
+        exit();
+}
+else {
+        printf("%s\n", $dbres);
+}
+
+$do = pg_Fetch_Object($dbres, 2);
+$name= $do->name;
+echo "and the name is... $name\n";
+
+
+?>
 </body>
 </html>
